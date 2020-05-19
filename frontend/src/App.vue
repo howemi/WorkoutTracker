5b92c851561a79b5a78393d5ababc44598396671
Chantel Prows <chantelp44@gmail.com>
2020-05-18 22:00:52 -0600

    front end template

---
+++ frontend/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <router-link to="/">
+        <v-btn
+          text
+        >
+          Home
+        </v-btn>
+      </router-link>
+      <router-link to="/workouts">
+        <v-btn
+                text
+        >
+          Workouts
+        </v-btn>
+      </router-link>
+    </v-app-bar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data: () => ({
+    //
+  })
+};
+</script>
---
+++ frontend/src/views/Workouts.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    WORKOUTS
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'workouts'
+  }
+</script>
